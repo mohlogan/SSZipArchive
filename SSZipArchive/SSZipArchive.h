62d75fc81192b63692d101a1b36495ca7e242220
Mohanraj Loganathan <mohlogan@in.ibm.com>
2018-04-17 16:56:25 +0530

    remove .h

--- SSZipArchive/SSZipArchive.h
+++ SSZipArchive/SSZipArchive.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double SSZipArchiveVersionNumber;
 FOUNDATION_EXPORT const unsigned char SSZipArchiveVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZipArchive/SSZipArchive.h>
-
-#import "SSZipArchiveImpl.h"
